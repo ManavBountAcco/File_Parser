1fa58d24c7379e49558f53daa4d6b493931c144b
Anish <anish.das@bounteous.com>
2025-05-26 15:55:15 +0530

    reading of file is done
    trial
    
    input.txt updated

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,14 @@ import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class Main {
-    public static void main(String[] args) {
-
-        //Code to parse By anish
-
+    public static void main(String[] args)  throws IOException {
+                Path filePath = Path.of(".//input.txt");
+                String content = Files.readString(filePath);
+                System.out.println(content);
         //code to read and generate By manav>
         //Approach : -> String -> O(n) read -> Add in Set -> add in output.txt if not generate else overwrite
 
